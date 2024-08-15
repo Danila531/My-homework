999f98db0b1630ae70df39b69062c0ec1b8c0e7e
Daniil <amelindaniil531@gmail.com>
2024-08-15 13:47:12 +0300

    добавил 2 js файла с использованием циклов

---
+++ app5-1.js
@@ -0,0 +1,18 @@
+// через FOR
+// for (let i = 20; i <= 30; i += 0.5) {
+// 	console.log(i);
+// }
+
+// через DO WHILE
+// let i = 20;
+// do {
+// 	console.log(i);
+// 	i += 0.5;
+// } while (i <= 30);
+
+// через WHILE
+let a = 20;
+while (a <= 30) {
+	console.log(a);
+	a += 0.5;
+}
